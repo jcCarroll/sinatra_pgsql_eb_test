fe004353decfb1752e4262591c0e446d9c8cf1e0
2staffi@gmail.com <2staffi@gmail.com>
2017-09-21 11:43:17 -0400

    Fixed db connection

--- app.rb
+++ app.rb
@@ -3,24 +3,29 @@ require 'pg'
 
 load './local_env.rb' if File.exists?('./local_env.rb')
 
-db_params = {
-    host: ENV['dbhost'],
-    port: ENV['dbport'],
-    dbname: ENV['dbname'],
-    user: ENV['dbuser'],
-    password: ENV['dbpassword']
-	}
+def connection()
+    db_params = {
+        host: ENV['dbhost'],
+        port: ENV['dbport'],
+        dbname: ENV['dbname'],
+        user: ENV['dbuser'],
+        password: ENV['dbpassword']
+        }
 
-db = PG::Connection.new(db_params)
+    db = PG::Connection.new(db_params)
+end
 
 get '/' do
     erb :index
 end
 
-post 'add_info' do
+post '/add_info' do
     first = params[:fname]
     last = params[:lname]
 
-    db.exec ("INSERT INTO names (first, last) VALUES ('#{first}', '#{last}'" )
+    db = connection()
+    db.exec ("INSERT INTO names (first, last) VALUES ('#{first}', '#{last}')")
     db.close
+
+    redirect '/'
 end
