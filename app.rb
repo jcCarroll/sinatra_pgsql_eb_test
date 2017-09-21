a3ee44edfa6530917debf44b7286ad08e0f344e2
2staffi@gmail.com <2staffi@gmail.com>
2017-09-21 10:56:53 -0400

    set up pg db

--- app.rb
+++ app.rb
@@ -1,6 +1,17 @@
 require 'sinatra'
+require 'pg'
 
 load './local_env.rb' if File.exists?('./local_env.rb')
+
+db_params = {
+    host: ENV['dbhost'],
+    port: ENV['dbport'],
+    dbname: ENV['dbname'],
+    user: ENV['dbuser'],
+    password: ENV['dbpassword']
+	}
+
+db = PG::Connection.new(db_params)
 
 get '/' do
     erb :index
@@ -9,4 +20,7 @@ end
 post 'add_info' do
     first = params[:fname]
     last = params[:lname]
+
+    db.exec ("INSERT INTO names (first, last) VALUES ('#{first}', '#{last}'" )
+    db.close
 end
