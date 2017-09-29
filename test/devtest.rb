a847196940d3f00fbd20d043095fc23771447413
2staffi@gmail.com <2staffi@gmail.com>
2017-09-29 10:03:55 -0400

    test for input and lables and submit

--- test/devtest.rb
+++ test/devtest.rb
@@ -6,12 +6,14 @@ class SelTestCase < Test::Unit::TestCase
 
   def setup
     caps = Selenium::WebDriver::Remote::Capabilities.chrome(chromeOptions: { args: [ "--headless" ]})
-    @driver = Selenium::WebDriver.for:chrome, desired_capabilities:caps
+    @driver = Selenium::WebDriver.for:chrome
+    # , desired_capabilities:caps
     @driver.navigate.to("http://devenv.j8fpymzrg5.us-west-2.elasticbeanstalk.com/")
     @wait = Selenium::WebDriver::Wait.new(:timeout => 5)
   end  
 
   def teardown
+    sleep 3
     @driver.close
   end
 
@@ -20,14 +22,20 @@ class SelTestCase < Test::Unit::TestCase
   end
 
   def test_name_fields
-    # First Name Label
     assert @wait.until { @driver.find_element(:xpath, '/html/body/form/table/tbody/tr[1]/td[1]/label').displayed? == true }
-    # First Name Input
     assert @wait.until { @driver.find_element(:xpath, '/html/body/form/table/tbody/tr[1]/td[2]/input').displayed? == true }
-    # Last Name Label
     assert @wait.until { @driver.find_element(:xpath, '/html/body/form/table/tbody/tr[2]/td[1]/label').displayed? == true }
-    # Last Name Input
     assert @wait.until { @driver.find_element(:xpath, '/html/body/form/table/tbody/tr[2]/td[2]/input').displayed? == true }
+  end
+
+  def test_submit_form
+    assert @wait.until { @driver.find_element(:xpath, '/html/body/form/input').displayed? == true }
+  end
+
+  def test_input_names_submit
+    @driver.find_element(:xpath, '/html/body/form/table/tbody/tr[1]/td[2]/input').send_keys('Jacob')
+    @driver.find_element(:xpath, '/html/body/form/table/tbody/tr[2]/td[2]/input').send_keys('Carroll')
+    @driver.find_element(:xpath, '/html/body/form/input').click()
   end
 
 end
